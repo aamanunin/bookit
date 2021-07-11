9b5928acb8ec67e035dfc4c4d977e45ba226e523
Aleksandr Maniunin <aamanunin@gmail.com>
2021-07-11 16:10:21 +0700

    Add locale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { AppLocaleModule } from './app-locale.module';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, AppLocaleModule],
   providers: [],
   bootstrap: [AppComponent],
 })
