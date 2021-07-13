2d2006f50ec677e6701b1ae1f2f0af21891f7983
Aleksandr Maniunin <aamanunin@gmail.com>
2021-07-13 23:49:23 +0700

    Add memory storage.

---
+++ src/app/core/storage/storages/memory.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MemoryService implements Storage {
+  private data: Record<string, any> = {};
+
+  get length(): number {
+    return Object.keys(this.data).length;
+  }
+
+  clear(): void {
+    this.data = {};
+  }
+
+  getItem(key: string): string | null {
+    return key in this.data ? this.data[key] : null;
+  }
+
+  key(index: number): string | null {
+    const keys = Object.keys(this.data);
+
+    return index >= 0 && keys.length < index ? keys[index] : null;
+  }
+
+  removeItem(key: string): void {}
+
+  setItem(key: string, value: string): void {
+    this.data[key] = value;
+  }
+}
